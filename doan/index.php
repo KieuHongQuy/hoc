20659a4917625ec33cacb08aee6af5d1fb3d2600
KieuHongQuy <kieuhongquy@gmail.com>
2020-07-14 19:42:49 +0700

    xoavinhvien

--- doan/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	session_start();
-	include_once "./config/config.php";
-	include_once './config/classdatabase.php';
-	include_once "./config/router.php";
-	include_once "./controller/custom.php";
-	include_once "./index_website.php";
-?>
